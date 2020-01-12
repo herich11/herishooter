d763d35838449c4c9006dce4c7e1dd92c13131e5
unknown <hericahyononew@gmail.com>
2020-01-12 08:48:25 +0700

    first commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1578793211,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pesawat-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/meteor-sheet0.png",
+		"images/ledakanmeteor.png",
+		"images/ledakanpesawat.png",
+		"images/bgscore-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/uigameover-sheet0.png",
+		"images/judul-sheet0.png",
+		"images/play-sheet0.png",
+		"media/sounddestroy.ogg",
+		"media/soundpeluru.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
